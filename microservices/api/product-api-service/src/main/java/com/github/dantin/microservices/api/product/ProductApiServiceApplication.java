b0d52ae5ae0c31643f4d704b4728ab0d2e3fae8c
David Ding <chengjie.ding@gmail.com>
2019-08-12 21:36:35 +0800

    update product-api to support oauth2 token relay. (#24)

--- microservices/api/product-api-service/src/main/java/com/github/dantin/microservices/api/product/ProductApiServiceApplication.java
+++ microservices/api/product-api-service/src/main/java/com/github/dantin/microservices/api/product/ProductApiServiceApplication.java
@@ -4,16 +4,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableResourceServer
 public class ProductApiServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductApiServiceApplication.class, args);
+    }
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+                    .and()
+                    .requestMatchers().antMatchers("/**")
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/**").authenticated()
+                    .and()
+                    .httpBasic();
+        }
     }
 
 }
