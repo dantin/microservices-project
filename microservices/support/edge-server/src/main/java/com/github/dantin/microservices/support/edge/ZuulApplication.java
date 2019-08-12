d50940334f5acee0c73909aa317c1485af727f3b
David Ding <chengjie.ding@gmail.com>
2019-08-12 21:33:13 +0800

    support oauth2 token relay. (#23)

--- microservices/support/edge-server/src/main/java/com/github/dantin/microservices/support/edge/ZuulApplication.java
+++ microservices/support/edge-server/src/main/java/com/github/dantin/microservices/support/edge/ZuulApplication.java
@@ -1,18 +1,34 @@
 package com.github.dantin.microservices.support.edge;
 
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-@Controller
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ZuulApplication.class).web(WebApplicationType.SERVLET).run(args);
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    @EnableOAuth2Sso
+    protected static class EdgeServerConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+        }
+
     }
 
 }
